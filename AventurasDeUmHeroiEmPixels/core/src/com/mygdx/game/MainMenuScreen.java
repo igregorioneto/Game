ddf3d89ca3f33206f24e83a2ddb950b17b95b868
gregorio <gregorio.neto@techsolutio.com>
2023-10-30 10:55:26 -0300

    Estudando e realizando a primeira aplicação com LibGDX

--- AventurasDeUmHeroiEmPixels/core/src/com/mygdx/game/MainMenuScreen.java
+++ AventurasDeUmHeroiEmPixels/core/src/com/mygdx/game/MainMenuScreen.java
@@ -2,10 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -21,18 +18,42 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 public class MainMenuScreen implements Screen {
     private MainGame game;
     private OrthographicCamera camera;
+    private SpriteBatch batch;
+
     private Texture buttonStart;
     private Texture mouse;
-    private SpriteBatch batch;
+
     public MainMenuScreen(MainGame game) {
         this.game = game;
         this.camera = new OrthographicCamera();
+        this.batch = new SpriteBatch();
         this.camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
         this.buttonStart = new Texture("button_start.png");
         this.mouse = new Texture("mouse.png");
-        this.batch = new SpriteBatch();
+
+        // Mouse
+        mouseCursor();
     }
+
+    private void mouseCursor() {
+        int imageWidth = 16;
+        int imageHeight = 16;
+        float scale = 2f;
+
+        int regionX = 0;
+        int regionY = 0;
+        int regionWidth = (int) (imageWidth);
+        int regionHeight = (int) (imageHeight);
+
+        TextureRegion cursorRegion = new TextureRegion(mouse, regionX, regionY, regionWidth, regionHeight);
+        cursorRegion.getTexture().getTextureData().prepare();
+
+        Pixmap cursorPixmap = new Pixmap(regionWidth, regionHeight, Pixmap.Format.RGBA8888);
+        cursorPixmap.drawPixmap(cursorRegion.getTexture().getTextureData().consumePixmap(), 0, 0, regionX, regionY, regionWidth, regionHeight);
+        Gdx.graphics.setCursor(Gdx.graphics.newCursor(cursorPixmap, regionWidth / 2, regionHeight / 2));
+    }
+
     @Override
     public void show() {
     }
@@ -41,8 +62,12 @@ public class MainMenuScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        float buttonX = Gdx.graphics.getWidth() / 2 - this.buttonStart.getWidth() / 2;
+        float buttonY = Gdx.graphics.getHeight() / 2 - this.buttonStart.getHeight() / 2;
+
         this.batch.begin();
-        this.batch.draw(this.buttonStart, (Gdx.graphics.getWidth() / 2 - this.buttonStart.getWidth() / 2), (Gdx.graphics.getHeight() / 2 - this.buttonStart.getHeight() / 2));
+        this.batch.draw(this.buttonStart, buttonX, buttonY);
         this.batch.end();
     }
 
