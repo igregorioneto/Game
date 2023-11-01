70be30f5f3a2e0cadc5717a1f627502202525922
gregorio <gregorio.neto@techsolutio.com>
2023-10-31 21:49:32 -0300

    Estudando a parte de Actor

--- StarfishCollectorV1/desktop/src/com/mygdx/game/LaucherAlpha.java
+++ StarfishCollectorV1/desktop/src/com/mygdx/game/LaucherAlpha.java
@@ -2,11 +2,15 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class LaucherAlpha {
     public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Starfish Collector V1");
+        config.setWindowedMode(800,600);
         Game myGame = new StarfishCollectorAlpha();
         Lwjgl3Application laucher =
-                new Lwjgl3Application(myGame);
+                new Lwjgl3Application(myGame, config);
     }
 }
