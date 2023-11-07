218bfd1c0f88140842cf015a6e629e0241954bec
gregorio <gregorio.neto@techsolutio.com>
2023-11-06 22:14:18 -0300

    Adicionando Métodos para Animação de Sprites

--- StarfishCollectorV2/core/src/com/mygdx/game/BaseActor.java
+++ StarfishCollectorV2/core/src/com/mygdx/game/BaseActor.java
@@ -1,14 +1,136 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.ArrayList;
 
 public class BaseActor extends Actor {
+    private Animation<TextureRegion> animation;
+    private float elapsedTime;
+    private boolean animationPaused;
+
     public BaseActor(float x, float y, Stage s) {
         // call constructor from Actor class
         super();
         // perform additional initialization tasks
         setPosition(x, y);
         s.addActor(this);
+
+        // Initialize these variables
+        animation = null;
+        elapsedTime = 0;
+        animationPaused = false;
     }
+
+    // Set the animation
+    public void setAnimation(Animation<TextureRegion> anim) {
+        animation = anim;
+        TextureRegion tr = animation.getKeyFrame(0);
+        float w = tr.getRegionWidth();
+        float h = tr.getRegionHeight();
+        setScale(w, h);
+        setOrigin(w / 2, h / 2);
+    }
+
+    // Paused Animation
+    public void setAnimationPaused(boolean paused) {
+        animationPaused = paused;
+    }
+
+    // Long animation has been playing
+    public void act(float dt) {
+        super.act(dt);
+
+        if (!animationPaused)
+            elapsedTime += dt;
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+
+        // apply color tint effect
+        Color c = getColor();
+        batch.setColor(c.r, c.g, c.b, c.a);
+
+        if (animation != null && isVisible())
+            batch.draw(animation.getKeyFrame(elapsedTime),
+                    getX(), getY(), getOriginX(), getOriginY(),
+                    getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+    }
+
+    public Animation<TextureRegion> loadAnimationFromFiles(
+            String[] fileNames, float frameDuration, boolean loop) {
+        int fileCount = fileNames.length;
+        Array<TextureRegion> textureArray = new Array<>();
+
+        for (int i = 0; i < fileCount; i++) {
+            String fileName = fileNames[i];
+            Texture texture = new Texture(Gdx.files.internal(fileName));
+            texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+            textureArray.add(new TextureRegion(texture));
+        }
+
+        Animation<TextureRegion> anim = new Animation<>(frameDuration, textureArray);
+        if (loop)
+            anim.setPlayMode(Animation.PlayMode.LOOP);
+        else
+            anim.setPlayMode(Animation.PlayMode.NORMAL);
+
+        if (animation == null)
+            setAnimation(anim);
+
+        return anim;
+    }
+
+    public Animation<TextureRegion> loadAnimationFromSheet(
+            String fileName, int rows, int cols, float frameDuration, boolean loop
+    ) {
+        Texture texture = new Texture(Gdx.files.internal(fileName));
+        texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        int frameWidth = texture.getWidth() / rows;
+        int framwHeight = texture.getHeight() / cols;
+
+        TextureRegion[][] temp = TextureRegion.split(texture, frameWidth, framwHeight);
+
+        Array<TextureRegion> textureArray = new Array<>();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                textureArray.add(temp[i][j]);
+            }
+        }
+
+        Animation<TextureRegion> anim = new Animation<>(frameDuration, textureArray);
+
+        if (loop)
+            anim.setPlayMode(Animation.PlayMode.LOOP);
+        else
+            anim.setPlayMode(Animation.PlayMode.NORMAL);
+
+        if (animation == null) {
+            setAnimation(anim);
+        }
+
+        return anim;
+    }
+
+    public Animation<TextureRegion> loadTexture(String fileName) {
+        String[] fileNames = new String[1];
+        fileNames[0] = fileName;
+        return loadAnimationFromFiles(fileNames, 1, true);
+    }
+
+    public boolean isAnimationFinished() {
+        return animation.isAnimationFinished(elapsedTime);
+    }
+
 }
