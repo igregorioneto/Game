0fd05751f02002aff38d101fc9d6576a55ade754
gregorio <igregoriodoneto@outlook.com>
2023-11-14 15:58:41 -0300

    Adicionando movimentação do personagem

--- LibGDX/LearnLibGDX/core/src/com/mygdx/game/TexturasESprites/TexturasESprites.java
+++ LibGDX/LearnLibGDX/core/src/com/mygdx/game/TexturasESprites/TexturasESprites.java
@@ -1,16 +1,12 @@
 package com.mygdx.game.TexturasESprites;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-
-import java.nio.ByteBuffer;
 
 public class TexturasESprites extends ApplicationAdapter {
     private Texture texture;
@@ -18,6 +14,8 @@ public class TexturasESprites extends ApplicationAdapter {
     private Sprite sprite;
 
     private float scale = 0.5f;
+    private float windowsWidth = 0;
+    private float windowsHeight = 0;
 
     @Override
     public void create() {
@@ -25,11 +23,17 @@ public class TexturasESprites extends ApplicationAdapter {
 
         // Carregar a imagem para transformar em uma textura
         texture = new Texture("assets/art/playerGrey_up1.png");
+
+        // Log para verificar a informação da imagem
         Gdx.app.log("INFO", "Largura da imagem: " + texture.getWidth());
         Gdx.app.log("INFO", "Altura da imagem: " + texture.getHeight());
 
+        windowsWidth = Gdx.graphics.getWidth();
+        windowsHeight = Gdx.graphics.getHeight();
+
         // Colocar a textura no sprite para transformar a textura em sprite
         sprite = new Sprite(texture);
+        sprite.setPosition(windowsWidth / 2,windowsHeight / 2);
     }
 
 
@@ -38,11 +42,36 @@ public class TexturasESprites extends ApplicationAdapter {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Chamando método para os inputs do personagem
+        handleInput();
+
         // Desenhar o sprite na tela com o SpriteBatch
         spriteBatch.begin();
-        spriteBatch.draw(sprite,100,100, 0,0, sprite.getWidth(), sprite.getHeight(), scale, scale, 0);
+        spriteBatch.draw(sprite,sprite.getX(),sprite.getY(), 0,0, sprite.getWidth(), sprite.getHeight(), scale, scale, 0);
         spriteBatch.end();
 
+    }
+
+
+    /*
+    * Método para movimentação do personagem
+    * Sem retorno
+    * */
+    private void handleInput() {
+        float moveSpeed = 200f * Gdx.graphics.getDeltaTime();
+
+        if (Gdx.input.isKeyPressed(Keys.LEFT))
+            sprite.translateX(-moveSpeed);
+        if (Gdx.input.isKeyPressed(Keys.RIGHT))
+            sprite.translateX(moveSpeed);
+        if (Gdx.input.isKeyPressed(Keys.UP))
+            sprite.translateY(moveSpeed);
+        if (Gdx.input.isKeyPressed(Keys.DOWN))
+            sprite.translateY(-moveSpeed);
+
+        // Limitando a posição do sprite na tela
+        sprite.setX(Math.max(0, Math.min(windowsWidth - sprite.getWidth() * scale, sprite.getX())));
+        sprite.setY(Math.max(0, Math.min(windowsHeight - sprite.getHeight() * scale, sprite.getY())));
     }
 
     @Override
