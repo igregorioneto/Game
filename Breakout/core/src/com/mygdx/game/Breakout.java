244bd94a5f947619566593b444883301b3babcb7
gregorio <igregoriodoneto@outlook.com>
2023-12-09 20:27:57 -0300

    feat: Construindo a base do jogo Breakout

---
+++ Breakout/core/src/com/mygdx/game/Breakout.java
@@ -0,0 +1,45 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Breakout extends ApplicationAdapter {
+	private ShapeRenderer shape;
+	private List<Ball> balls = new ArrayList<>();
+	private Random r = new Random();
+	
+	@Override
+	public void create () {
+		shape = new ShapeRenderer();
+		for (int i = 0; i < 10; i++) {
+			balls.add(new Ball(r.nextInt(Gdx.graphics.getWidth()),
+					r.nextInt(Gdx.graphics.getHeight()),
+					r.nextInt(100),
+					r.nextInt(15),
+					r.nextInt(15)));
+		}
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		shape.begin(ShapeRenderer.ShapeType.Filled);
+
+		for (Ball ball:balls) {
+			ball.update();
+			ball.draw(shape);
+		}
+
+		shape.end();
+	}
+}
